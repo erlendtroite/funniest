99d0ae876b28ef10d0f0ef0cfd680121a561a03f
erlendtroite <erlend.troite@embida.no>
2016-06-15 21:24:04 +0100

    Saving work

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
-# Setup script
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/erlendtroite/funniest',
+      author='Anders Binders',
+      author_email='anders@binders.com',
+      license='EAT',
+      #packages=['funniest'],
+      packages=find_packages(),
+      install_requires=[
+          'markdown',
+      ],
+      #dependency_links = ['http://github.com/erlendtroite/funniest/tarball/master#egg=funniest-0.1'],
+      include_package_data=True,
+      zip_safe=False)
